fa988d141ca41009e67a40211abc019dd0549e68
David Skinner <skinner.david@gmail.com>
2020-04-23 16:12:21 -0500

    Update installZoom.sh

--- scripts/installZoom.sh
+++ scripts/installZoom.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 cd /opt
 curl -LO https://zoom.us/client/latest/zoom_x86_64.tar.xz
 tar -xvf zoom*.tar.xz
 cd zoom
 ./ZoomLauncher
-cd <full-path-to-zoom>/RegisterProtocol
-echo "Icon=<full-path-to-zoom>/Zoom.png" >> ZoomLauncher.desktop
+cd /opt/zoom/RegisterProtocol
+echo "Icon=/opt/zoom/Zoom.png" >> ZoomLauncher.desktop
 # may need for HiDPI screens
 sed -i 's|^Exec=.*$|Exec=/usr/bin/env QT_SCALE_FACTOR=2 ZoomLauncher %U|' ZoomLauncher.desktop
 bash RegisterProtocol.sh
